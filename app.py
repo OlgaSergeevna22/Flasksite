0ea786d22e3c032ce2123a4ed5ba20896bf0a447
Ольга <devyatovka.0993@mail.ru>
2022-07-25 15:50:09 +0300

    add_comments

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -7,6 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///animal.db'
 app.config['QLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
@@ -14,34 +15,57 @@ class Comment(db.Model):
     date = db.Column(db.DateTime, default=datetime.now)
 
     def __repr__(self):
-        return '<Article %r' % self.id
+        return '<Comment %r' % self.id
 
 
-@app.route('/comment')
-def comment():
-    return render_template('comment.html')
+@app.route('/comment',  methods=['POST', 'GET'])
+def create_comment():
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
 
+        comment = Comment(title=title, text=text)
+
+        try:
+            db.session.add(comment)
+            db.session.commit()
+            return redirect('/all_comments')
+        except:
+            return 'Произошла ошибка'
+    else:
+        return render_template('comment.html')
+
+
+@app.route('/all_comments')
+def all_comments():
+    comments = Comment.query.order_by(Comment.date.desc()).all()
+    return render_template('all_comments.html', comments=comments)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/main-1')
 def main_1():
     return render_template('main1.html')
+
 
 @app.route('/main-2')
 def main_2():
     return render_template('main2.html')
 
+
 @app.route('/main-3')
 def main_3():
     return render_template('main3.html')
 
+
 @app.route('/main-4')
 def main_4():
     return render_template('main4.html')
+
 
 @app.route('/main-5')
 def main_5():
